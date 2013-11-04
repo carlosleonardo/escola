ef100d716db0364825e96c000507b23468064a44
carlosleonardo <carlos.leonardo.goes@gmail.com>
2013-11-04 14:52:59 -0300

    Inclusão de hashcode

--- src/main/java/org/serpro/demoiselle/escola/domain/AulaPK.java
+++ src/main/java/org/serpro/demoiselle/escola/domain/AulaPK.java
@@ -3,10 +3,17 @@ package org.serpro.demoiselle.escola.domain;
 import java.io.Serializable;
 import java.util.Date;
 
+import javax.persistence.Column;
 import javax.persistence.Embeddable;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
+
+/**
+ * 
+ * @author 86132113568
+ * Chave composta que será usada como ID da classe correspondente
+ */
 @SuppressWarnings("serial")
 @Embeddable
 public class AulaPK implements Serializable {
@@ -34,17 +41,6 @@ public class AulaPK implements Serializable {
 		this.idDisciplina = idDisciplina;
 	}
 	
-	@Override
-	public boolean equals(Object objeto) {
-		AulaPK outro = (AulaPK) objeto;
-		return (outro.idDisciplina==this.idDisciplina && outro.idProfessor == this.idProfessor &&
-				outro.horaInicio.equals(this.horaInicio) && outro.horaFim.equals(this.horaFim));
-	}
-	
-	@Override
-	public int hashCode() {
-		return super.hashCode();
-	}
 	public Date getHoraInicio() {
 		return horaInicio;
 	}
@@ -62,5 +58,56 @@ public class AulaPK implements Serializable {
 	}
 	public void setIdSala(Long idSala) {
 		this.idSala = idSala;
+	}
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((horaFim == null) ? 0 : horaFim.hashCode());
+		result = prime * result
+				+ ((horaInicio == null) ? 0 : horaInicio.hashCode());
+		result = prime * result
+				+ ((idDisciplina == null) ? 0 : idDisciplina.hashCode());
+		result = prime * result
+				+ ((idProfessor == null) ? 0 : idProfessor.hashCode());
+		result = prime * result + ((idSala == null) ? 0 : idSala.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		AulaPK other = (AulaPK) obj;
+		if (horaFim == null) {
+			if (other.horaFim != null)
+				return false;
+		} else if (!horaFim.equals(other.horaFim))
+			return false;
+		if (horaInicio == null) {
+			if (other.horaInicio != null)
+				return false;
+		} else if (!horaInicio.equals(other.horaInicio))
+			return false;
+		if (idDisciplina == null) {
+			if (other.idDisciplina != null)
+				return false;
+		} else if (!idDisciplina.equals(other.idDisciplina))
+			return false;
+		if (idProfessor == null) {
+			if (other.idProfessor != null)
+				return false;
+		} else if (!idProfessor.equals(other.idProfessor))
+			return false;
+		if (idSala == null) {
+			if (other.idSala != null)
+				return false;
+		} else if (!idSala.equals(other.idSala))
+			return false;
+		return true;
 	}
 }
