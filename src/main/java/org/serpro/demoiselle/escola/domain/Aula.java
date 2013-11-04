b05414b0467c67ebd023e5464e12d39750a19a89
carlosleonardo <carlos.leonardo.goes@gmail.com>
2013-11-04 14:53:34 -0300

    Inclusão de relacionamento Join com tabela Aluno

--- src/main/java/org/serpro/demoiselle/escola/domain/Aula.java
+++ src/main/java/org/serpro/demoiselle/escola/domain/Aula.java
@@ -3,8 +3,12 @@ package org.serpro.demoiselle.escola.domain;
 import java.io.Serializable;
 import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
 
 /**
  * Entity implementation class for Entity: Aula
@@ -16,6 +20,10 @@ public class Aula implements Serializable {
 	@EmbeddedId
 	private AulaPK idAula;
 
+	@OneToMany(cascade={CascadeType.ALL})
+	@JoinTable(name="Aluno")
+	private List<Aluno> alunos;
+	
 	public Aula() {
 		super();
 	}
@@ -26,6 +34,14 @@ public class Aula implements Serializable {
 
 	public void setIdAula(AulaPK idAula) {
 		this.idAula = idAula;
+	}
+
+	public List<Aluno> getAlunos() {
+		return alunos;
+	}
+
+	public void setAlunos(List<Aluno> alunos) {
+		this.alunos = alunos;
 	}
 
 }
