46bbe593ec2dbaeef3620ae977e5833203cbdd74
carlosleonardo <carlos.leonardo.goes@gmail.com>
2013-11-11 14:40:19 -0300

    Criação das classes de negócio CursoBC e AulaBC

---
+++ src/main/java/org/serpro/demoiselle/escola/business/CursoBC.java
@@ -0,0 +1,13 @@
+package org.serpro.demoiselle.escola.business;
+
+import org.serpro.demoiselle.escola.domain.Curso;
+import org.serpro.demoiselle.escola.domain.CursoPK;
+import org.serpro.demoiselle.escola.persistence.CursoDAO;
+
+import br.gov.frameworkdemoiselle.stereotype.BusinessController;
+import br.gov.frameworkdemoiselle.template.DelegateCrud;
+
+@SuppressWarnings("serial")
+@BusinessController
+public class CursoBC extends DelegateCrud<Curso, CursoPK, CursoDAO> {
+}
