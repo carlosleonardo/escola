a69cff9275372cbe2cb720eeb1fa8d20d377c586
carlosleonardo <carlos.leonardo.goes@gmail.com>
2013-11-12 15:57:15 -0300

    Removida referência a CursoPK e AulaPK

--- src/main/java/org/serpro/demoiselle/escola/persistence/AulaDAO.java
+++ src/main/java/org/serpro/demoiselle/escola/persistence/AulaDAO.java
@@ -1,12 +1,10 @@
 package org.serpro.demoiselle.escola.persistence;
 
 import org.serpro.demoiselle.escola.domain.Aula;
-import org.serpro.demoiselle.escola.domain.AulaPK;
-
 import br.gov.frameworkdemoiselle.stereotype.PersistenceController;
 import br.gov.frameworkdemoiselle.template.JPACrud;
 
 @SuppressWarnings("serial")
 @PersistenceController
-public class AulaDAO extends JPACrud<Aula, AulaPK> {
+public class AulaDAO extends JPACrud<Aula, Long> {
 }
--- src/main/java/org/serpro/demoiselle/escola/persistence/CursoDAO.java
+++ src/main/java/org/serpro/demoiselle/escola/persistence/CursoDAO.java
@@ -1,12 +1,10 @@
 package org.serpro.demoiselle.escola.persistence;
 
 import org.serpro.demoiselle.escola.domain.Curso;
-import org.serpro.demoiselle.escola.domain.CursoPK;
-
 import br.gov.frameworkdemoiselle.stereotype.PersistenceController;
 import br.gov.frameworkdemoiselle.template.JPACrud;
 
 @SuppressWarnings("serial")
 @PersistenceController
-public class CursoDAO extends JPACrud<Curso, CursoPK> {
+public class CursoDAO extends JPACrud<Curso, Long> {
 }
