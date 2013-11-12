63e892a89fb475bb7c0011f3078cf33cd60fe27f
carlosleonardo <carlos.leonardo.goes@gmail.com>
2013-11-12 14:22:18 -0300

    Ajuste para corresponder ao modelo de dados

--- src/main/java/org/serpro/demoiselle/escola/domain/Professor.java
+++ src/main/java/org/serpro/demoiselle/escola/domain/Professor.java
@@ -5,34 +5,16 @@ import java.io.Serializable;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-import javax.persistence.OneToOne;
 
 /**
  * Entity implementation class for Entity: Professor
  *
  */
+@SuppressWarnings("serial")
 @Entity
 public class Professor extends Pessoa implements Serializable {
-	
-	private static final long serialVersionUID = 1L;
-
-	public Professor() {
-		super();
-	}
 
 	@Id
 	@GeneratedValue
 	private Long id;
-
-	@OneToOne
-	private Disciplina disciplina;
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-	
 }
