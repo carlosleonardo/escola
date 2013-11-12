9e91766e67e04ad519a082a5b9da98dd561e4c72
carlosleonardo <carlos.leonardo.goes@gmail.com>
2013-11-12 15:56:46 -0300

    Removida referência a CursoPK e a AulaPK

--- src/main/java/org/serpro/demoiselle/escola/business/AulaBC.java
+++ src/main/java/org/serpro/demoiselle/escola/business/AulaBC.java
@@ -1,13 +1,11 @@
 package org.serpro.demoiselle.escola.business;
 
 import org.serpro.demoiselle.escola.domain.Aula;
-import org.serpro.demoiselle.escola.domain.AulaPK;
 import org.serpro.demoiselle.escola.persistence.AulaDAO;
-
 import br.gov.frameworkdemoiselle.stereotype.BusinessController;
 import br.gov.frameworkdemoiselle.template.DelegateCrud;
 
 @SuppressWarnings("serial")
 @BusinessController
-public class AulaBC  extends DelegateCrud<Aula, AulaPK, AulaDAO> {
+public class AulaBC  extends DelegateCrud<Aula, Long, AulaDAO> {
 }
